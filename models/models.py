59054e09e4a85ffcbec870530a37fc9d9e9f719a
mubeen1020 <m.mubeen1020@gmail.com>
2024-10-18 16:16:04 +0500

    working

--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models,fields
 
 class CRMLead(models.Model):
     _inherit = 'crm.lead'
@@ -18,3 +18,34 @@ class CRMLead(models.Model):
             print ('ssssssssssssssssssssssssssssssss')
             args += [('company_id', '=', selected_company_id)]  # Ensure all company records are included
             return super(CRMLead, self).search(args, offset, limit, order, count)
+
+
+class CRMCreateWizard(models.TransientModel):
+    _name = 'crm.create.wizard'
+    _description = 'CRM Create Wizard'
+
+    company_id = fields.Many2one('res.company', string='Company', required=True,default=lambda self: self.env.user.company_id.id)
+    customer_price = fields.Float(string='Customer Price')
+    name = fields.Char(string='Name', required=True)
+    res_partner = fields.Many2one('res.partner', string='Customer', required=True)
+    rating = fields.Selection([
+        ('0', 'Low'),
+        ('1', 'Medium'),
+        ('2', 'High'),
+        ('3', 'Very High'),
+    ], string='Rating')
+
+    def create_crm_record(self):
+        lead = self.env['crm.lead'].sudo().create({
+        'company_id': self.company_id.id,
+        'partner_id': self.res_partner.id,
+        'planned_revenue': self.customer_price,
+        'priority': self.rating,
+        'name': self.name, 
+        'stage_id':1 ,
+        'Probability':10
+        })
+        return {
+        'type': 'ir.actions.client',
+        'tag': 'reload',
+        }
